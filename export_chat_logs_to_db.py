69446714697758e912dcec6439105a142b39f284
Daklyan <louistardy58@gmail.com>
2024-03-14 22:47:43 +0100

    adding some comments

--- export_chat_logs_to_db.py
+++ export_chat_logs_to_db.py
@@ -15,14 +15,14 @@ DB_NAME = "chatlogs_test"
 PATH_TO_LOGS = "/mnt/c/Users/bolos/AppData/Roaming/Chatterino2/Logs/Twitch/Channels/"
 
 class Database():
-    def __init__(self):
+    def __init__(self, db_user: str, db_pass: str, host: str, port: int, database: str):
         try:
             self.connection = mariadb.connect(
-                user=DB_USER,
-                password=DB_PASS,
-                host=DB_HOST,
-                port=DB_PORT,
-                database=DB_NAME
+                user=db_user,
+                password=db_pass,
+                host=host,
+                port=port,
+                database=database
             )
             self.cursor = self.connection.cursor()
         except mariadb.Error as err:
@@ -46,6 +46,14 @@ class Parser():
         self.cur = self.db.get_cursor()
 
     def check_if_file_is_done(self, filename: str) -> bool:
+        """Checks in database if file as already been exported
+
+        Args:
+            filename (str): Name of the file to check
+
+        Returns:
+            bool: Returns True if already done, if not returns False
+        """
         try:
             self.cur.execute(
                 "SELECT id FROM done_file WHERE file=?",
@@ -58,6 +66,14 @@ class Parser():
         return False
 
     def write_message_to_db(self, pseudo: str, message: str, channel: str, timestamp):
+        """Write logged message to database
+
+        Args:
+            pseudo (str): User pseudo
+            message (str): Message written in chat
+            channel (str): Channel it was written to
+            timestamp (_type_): Timestamp it was sent to
+        """
         try:
             self.cur.execute(
                 "INSERT INTO message (pseudo, message, channel, timestamp) VALUES (?, ?, ?, ?)",
@@ -68,6 +84,12 @@ class Parser():
             print(f"Error while adding message to database: {err}")
 
     def write_file_to_done(self, channel: str, filename: str):
+        """Append file as done to the database
+
+        Args:
+            channel (str): Name of the twitch channel
+            filename (str): File done exporting
+        """
         try:
             self.cur.execute(
                 "INSERT INTO done_file (channel, file) VALUES (?, ?)",
@@ -78,6 +100,14 @@ class Parser():
             print(f"Error while adding file to done in database: {err}")
 
     def parse_line(self, line: str):
+        """Parse a line to get the timestamp, pseudo and message out of it
+
+        Args:
+            line (str): Line of a chatterino log
+
+        Returns:
+            (str, str, str) | (None, None, None): Parsed values or None if it fails to
+        """
         try:
             timestamp = re.search(r'\[(.*?)\]', line).group(1)
             pseudo = re.search(r'] (.*?):', line).group(1)
@@ -87,6 +117,12 @@ class Parser():
         return timestamp.split(":"), pseudo, message
 
     def parse_file(self, dirpath: str, file: str):
+        """Go through file of a given a path, parse it and call to write in database
+
+        Args:
+            dirpath (str): Path to file
+            file (str): Filename to parse
+        """
         path = os.path.join(dirpath, file)
         channel = file.split('-')[0]
         date_us = file.replace(f"{channel}-", "")[:-4].split('-')
@@ -120,13 +156,15 @@ class Parser():
             self.write_file_to_done(channel=channel, filename=file)
 
     def parse_and_write_to_db(self):
+        """Go through every file of a given path and starts the parsing / writing to database
+        """
         for dirpath, _, files in os.walk(PATH_TO_LOGS):
             for file in files:
                 print(f"Processing {os.path.join(dirpath, file)} ...")
                 self.parse_file(dirpath, file)
 
 
-database = Database()
+database = Database(db_user=DB_USER, db_pass=DB_PASS, host=DB_HOST, port=DB_PORT, database=DB_NAME)
 parser = Parser(database)
 
 parser.parse_and_write_to_db()
